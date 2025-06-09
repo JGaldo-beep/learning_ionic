8735985920f95607970c8ca7296d892485fb14da
silvana1972 <silvana.bamba@ucb.edu.bo>
2025-06-08 22:22:15 -0400

    feat: se crea la página silvana

---
+++ src/app/silvana/silvana.page.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonGrid,
+  IonRow,
+  IonCol
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-silvana',
+  templateUrl: './silvana.page.html',
+  styleUrls: ['./silvana.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonGrid,
+    IonRow,
+    IonCol
+  ]
+})
+export class SilvanaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
